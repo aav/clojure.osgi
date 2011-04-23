d3a08ee3e1e655be5c562674ea8c02ef167593be
Alexey Aristov <aav@acm.org>
2011-04-23 20:33:01 +0200

    OSGi classloading constraints properly enforced. package that is not imported, will be fail to load, even if it is already loaded and cached by clojure

--- clojure.osgi/src/main/clojure/clojure/osgi/core.clj
+++ clojure.osgi/src/main/clojure/clojure/osgi/core.clj
@@ -1,15 +1,28 @@
 (ns clojure.osgi.core
 (:import [clojure.osgi.internal BundleClassLoader]))
 
-(def *bundle* nil)
 
-(defmacro with-captured-bundle [& body]
-  `(let [~'*bundle* *bundle*]
-     (do ~@body)
-   ) 
-)
+(def ^{:private true} osgi-debug false)
+
+(def *bundle*)
 
 ; copy from clojure.core BEGIN
+(defn- libspec?
+  "Returns true if x is a libspec"
+  [x]
+  (or (symbol? x)
+      (and (vector? x)
+           (or
+            (nil? (second x))
+            (keyword? (second x))))))
+
+(defn- prependss
+  "Prepends a symbol or a seq to coll"
+  [x coll]
+  (if (symbol? x)
+    (cons x coll)
+    (concat x coll)))
+
 (defn- root-resource
   "Returns the root directory path for a lib"
   {:tag String}
@@ -25,7 +38,6 @@
   (let [d (root-resource lib)]
     (subs d 0 (.lastIndexOf d "/"))))
 
-
 (defonce
   ^{:private true
     :doc "the set of paths currently being loaded by this thread"}
@@ -34,59 +46,168 @@
 ; copy from clojure.core - END
 
 
-(with-captured-bundle
+(defn- full-path [path]
+	(if (.startsWith path "/")
+	  path
+	  (str (root-directory (ns-name *ns*)) \/ path))
+)
+
+
+(defonce 
+  ^{:private true}
+  *currently-loading* nil)
+
+
+(let [bundle *bundle*]
 	(defn bundle-name []
-	  (.getSymbolicName *bundle*)
+	  (.getSymbolicName bundle)
 	)
 
 	(defn get-bundle [bid]
-    (.. *bundle* (getBundleContext) (getBundle bid)) 
+    (.. bundle (getBundleContext) (getBundle bid)) 
 	)
 )
 
-(defn bundle-class-loader [bundle]
-  (BundleClassLoader. bundle)
+(defn- libspecs [args]
+  (flatten 
+    (map 
+	    (fn [arg]
+	      (cond
+	        (keyword arg) 
+	          arg
+	
+	        (and (vector? arg) (or (nil? (second arg)) (keyword? (second arg)))) 
+	          (first arg)
+	
+	        :default
+	          (let [[prefix & args] arg]
+	             (map #(str (name prefix) "." (name %)) args))
+	      )
+	    )
+
+      (filter (complement keyword?) args)
+    )
+  )
 )
 
-(declare osgi-load)
+(defn- available [lib]
+  (or
+     (let [cname (str lib "__init")]
+       (try
+	       (.loadClass *bundle* cname)
+	       (catch ClassNotFoundException _
+           (when osgi-debug
+             (println "class not found: " cname))
+         )
+       )
+     )
+     
+     (let [rname (str (root-resource lib) ".clj")] 
+	     (or 
+         (.getResource *bundle* rname)
+         (when osgi-debug
+           (println "resource not found: " rname))
+       )
+     )
+  )
+)
 
+(defn check-libs [libs]
+  (doseq [lib libs]
+    (when-not (available lib)
+      (when osgi-debug
+        (println (str lib " is not available from bundle " (.getSymbolicName *bundle*))))
+
+      (throw (Exception. (str "cannot load " lib " from bundle " (.getSymbolicName *bundle*))))
+    )
+  )
+)
+
+; override clojure.core/use
+(let [original use]
+	(defn- osgi-use [& args]
+	  (when osgi-debug
+	 	  (println (str "use " args " from " (.getSymbolicName *bundle*) *currently-loading*)))
+	
+	  (check-libs (libspecs args))
+	
+	  (apply original args)
+	)
+)
+
+(let [original require]
+	(defn- osgi-require [& args]
+    (when osgi-debug
+      (println (str "require " args " from " (.getSymbolicName *bundle*) *currently-loading*)))
+
+	  (check-libs (libspecs args))
+
+	  (apply original args)
+	)
+)
+
+(declare with-bundle*)
+(defmulti bundle-for-resource (constantly (System/getProperty "org.osgi.framework.vendor")))
+
+; extracts bundle id from eclipse/equinox resource url 
+(defn- eclipse-bundle-id [url]
+	(let [host (.getHost url) dot (.indexOf host  (int \.))]
+	  (Integer/parseInt
+	    (if (and (>= dot 0) (< dot (- (count host) 1)))
+	      (.substring host 0 dot) host))
+	)
+)
+
+; computes bundle that is able to provide resource, specified by it's name
+(defmethod bundle-for-resource "Eclipse" [bundle resource]
+  (if-let [url (.getResource bundle resource)]
+    (.getBundle (.getBundleContext *clojure-osgi-bundle*) (eclipse-bundle-id url))
+  )
+)
+
+
+
+; OSGi version of clojure's load 
+(defn- osgi-load [path]
+  (when osgi-debug
+    (println (str "load " path " from " (.getSymbolicName *bundle*))))
+ 
+  (let [path (full-path path)]
+	  (if-not (*pending-paths* path)
+		  (binding [
+                *pending-paths* (conj *pending-paths* path)
+                *currently-loading* path]
+        (let [load (fn [] (clojure.lang.RT/load (.substring path 1)))]
+				  (if-let [bundle (bundle-for-resource *bundle* (str path ".clj"))]
+            (with-bundle* bundle load)					    
+				    (load))
+        )
+ 	    )
+	  )
+  )
+)
+
+; invokes function in the environment set-up for specified bundle:
+;   - classloader is is set to appropriate bundle class loader
+;   - clojure.core/load is re-bound with osgi-load
+;   - clojure.core/use is re-bound with osgi-use
 (defn with-bundle* [bundle function & params]
-  (binding [*bundle* bundle load (osgi-load bundle)]
-     (clojure.osgi.internal.ClojureOSGi/withLoader (bundle-class-loader bundle) 
+  (binding [*bundle* bundle 
+            load osgi-load
+            use  osgi-use
+            require osgi-require]
+     (clojure.osgi.internal.ClojureOSGi/withLoader (BundleClassLoader. bundle) 
        (if (seq? params)
          (apply partial (cons function params)) function)
      )
   )   
 )
 
-
+; convinience macro
 (defmacro with-bundle [bundle & body]
   `(with-bundle* ~bundle
       (fn [] ~@body)
    )
-)
-
-(defmulti bundle-for-resource (constantly (System/getProperty "org.osgi.framework.vendor")))
-
-(defmethod bundle-for-resource "Eclipse" [bundle resource]
-  (letfn 
-    [
-      (bundle-id [url]
-				(let [host (.getHost url) dot (.indexOf host  (int \.))]
-				  (Integer/parseInt
-				    (if (and (>= dot 0) (< dot (- (count host) 1)))
-				      (.substring host 0 dot)
-				      host
-				    ) 
-				  )
-				)
-      )
-    ]
-
-	  (if-let [url (.getResource bundle resource)]
-	    (.getBundle (.getBundleContext *clojure-osgi-bundle*) (bundle-id url))
-	  )
-  )
 )
 
 (defn load-aot-class [bundle-context class-name]
@@ -95,27 +216,4 @@
                    true 
                    (BundleClassLoader. 
                      (.getBundle bundle-context))))
-) 
-
-(defn- osgi-load [bundle]
-  (fn [path]
-	  (let [^String path (if (.startsWith path "/")
-	    path
-	    (str (root-directory (ns-name *ns*)) \/ path))]
-	
-		  (if-not (*pending-paths* path)
-			  (binding [*pending-paths* (conj *pending-paths* path)]
-          (let [load (fn [] (clojure.lang.RT/load (.substring path 1)))]
-					  (if-let [bundle (bundle-for-resource bundle (str path ".clj"))]
-              (with-bundle* bundle load)					    
-					    (load))
-          )
-	 	    )
-		  )
-	  )
-  )
 )
-
-
-
-
